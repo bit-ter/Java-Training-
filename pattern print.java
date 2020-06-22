2e3c1cfca1eac2d03cc5c151e7c272a126e2172a
bitumour <44092668+bit-ter@users.noreply.github.com>
2020-06-22 16:01:38 +0530

    Create pattern print.java

---
+++ pattern print.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    for(int i=0;i<5;i++){
+	        for(int j=0;j<5;j++){
+	            if(j<=i)
+	                System.out.print(j+1+" ");
+	        }
+	        System.out.println("");
+	    }
+	}
+}
