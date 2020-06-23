f34cbae1ebb750c4a13bec2c52e3969e44e81fe7
Devnath E S <44092668+bit-ter@users.noreply.github.com>
2020-06-23 16:25:47 +0530

    Update Test1.java

--- myPackage/Test1.java
+++ myPackage/Test1.java
@@ -1,5 +1,13 @@
 import java.io.*;
-
-class Test1{
-
+import myPackage2.Test2;
+class Test1 extends Test2{
+    public static void main(String[] args){
+        Test1 t=new Test1();
+        t.show();
+        //t.display(); //error as Test2.display is protected
+        t.showDisplay();
+    }
+    void showDisplay(){
+        this.display();
+    }
 }
