025c9842dd5ea343f1bbd96bfe667fedc52354da
Devnath E S <44092668+bit-ter@users.noreply.github.com>
2020-06-23 16:22:08 +0530

    Create Test2.java

---
+++ myPackage2/Test2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Test2{
+    public void show(){
+        System.out.println("inside public show() of Test2");
+    }
+    protected void display(){
+        System.out.println("inside protected display() of Test2");
+    }
+}
